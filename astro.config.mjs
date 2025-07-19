3e8354b5c82567c083ccd3aaf21fd475303a4f58
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-07-19 11:25:08 +0800

    feat: add heading anchor link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,24 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import mermaid from 'astro-mermaid';
 import { defineConfig } from "astro/config";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://amoshydra.github.io",
   base: "/blog",
-  integrations: [mdx(), sitemap(), mermaid({
-    theme: 'forest',
-    autoTheme: true
-  })],
+  markdown: {
+    remarkPlugins: [ [remarkToc, { heading: "contents"} ] ],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'prepend' }]],
+  },
+  integrations: [
+    mdx(),
+    sitemap(),
+    mermaid({
+      theme: 'forest',
+      autoTheme: true,
+    })
+  ],
 });
