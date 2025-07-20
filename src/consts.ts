1d34000c5434375f3bdf8bdf1d56843e1ca90ac0
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-07-20 23:25:26 +0800

    feat: update cover image, favion and site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "amoshydra blog";
-export const SITE_DESCRIPTION = "amoshydra blog";
+export const SITE_DESCRIPTION = "Dive into amoshydra's blog to explore a wide array of topics";
